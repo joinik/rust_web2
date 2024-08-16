c23784402b9b69a803f9e9e8ae7e4f5337922cfb
joinik <1793844527@qq.com>
2024-08-16 03:20:41 +0000

    读取请求

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::net::TcpListener;
-
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // 监听地址：127.0.0.1:7878
@@ -8,6 +10,19 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection from {}", stream.peer_addr().unwrap())
+        handle_connection(stream);
     }
+}
+
+// 处理连接
+fn handle_connection(mut stream: TcpStream) {
+    // 读取客户端发送的数据
+    let buffer = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buffer
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    // 处理数据
+    println!("Request: {:#?}", http_request);
 }
