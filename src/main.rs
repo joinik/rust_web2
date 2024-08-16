f69e598d3a166eada2f33a4b58d9cd3a99d4f600
joinik <1793844527@qq.com>
2024-08-16 03:03:03 +0000

    监听Tcp连接

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::net::TcpListener;
+
+
+fn main() {
+    // 监听地址：127.0.0.1:7878
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Listening on IP_ADDRESS:7878");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection from {}", stream.peer_addr().unwrap())
+    }
+}
